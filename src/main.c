f10693bf5760479b657ee4d556af748f0b918ba2
Merlin Kooshmanian <mkooshmanian@gmail.com>
2023-01-07 11:11:44 +0100

    Blink fonctionnel pour stm32f407

--- src/main.c
+++ src/main.c
@@ -16,10 +16,10 @@
 #include "sysclock.h"
 
 /************************** Constant Definitions *****************************/
-#define LED4_PIN                         0x1000
-#define LED3_PIN                         0x2000
-#define LED5_PIN                         0x4000
-#define LED6_PIN                         0x8000
+#define LED3_PIN                         13U
+#define LED4_PIN                         12U
+#define LED5_PIN                         14U
+#define LED6_PIN                         15U
 
 /**************************** Type Definitions *******************************/
 
@@ -35,11 +35,22 @@ int main(){
     clock_init();
 
     RCC->AHB1ENR |= (1 << RCC_AHB1ENR_GPIODEN_Pos);     /* Enable clock on PORTD	    */
-    GPIOD->MODER |= (0x1 << (LED5_PIN*2));               /* Set LED pin as OUTPUT	    */
-    GPIOD->ODR |= (1 << LED5_PIN);		                /* Set LED output to 1		    */
+    
+    GPIOD->MODER |= (0x1 << (LED4_PIN*2));               /* Set LED4 pin as OUTPUT	    */
+    GPIOD->MODER |= (0x1 << (LED5_PIN*2));               /* Set LED5 pin as OUTPUT	    */
+    GPIOD->MODER |= (0x1 << (LED3_PIN*2));               /* Set LED4 pin as OUTPUT	    */
+    GPIOD->MODER |= (0x1 << (LED6_PIN*2));               /* Set LED5 pin as OUTPUT	    */
+
+    GPIOD->ODR |= (1 << LED4_PIN);		                /* Set LED4 output to 1		    */
+    GPIOD->ODR |= (1 << LED5_PIN);		                /* Set LED5 output to 1		    */
+    GPIOD->ODR |= (0 << LED3_PIN);		                /* Set LED3 output to 0		    */
+    GPIOD->ODR |= (0 << LED6_PIN);		                /* Set LED6 output to 0		    */
 
     while(1){
-	    GPIOD->ODR ^= (1 << LED5_PIN);		            /* Toggle the led			    */	
+        GPIOD->ODR ^= (1 << LED4_PIN);		            /* Toggle LED4		    	    */	
+	    GPIOD->ODR ^= (1 << LED5_PIN);		            /* Toggle LED5		    	    */
+        GPIOD->ODR ^= (1 << LED3_PIN);		            /* Toggle LED3		    	    */	
+	    GPIOD->ODR ^= (1 << LED6_PIN);		            /* Toggle LED6		    	    */		
 	    delay_ms(500);                                  /* 500ms delay for 1Hz blinking */
     }
 }
